427075006767456bbc4dc79a62e21a00f855876f
nhanlam1792 <157191018+nhanlam1792@users.noreply.github.com>
2024-05-31 11:55:48 +0700

    Update

---
+++ Data/Migrations/20240528065625_AddAdminUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserManagementWithIdentity.Data.Migrations
+{
+    public partial class AddAdminUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO [security].[Users] ([Id], [UserName], [NormalizedUserName], [Email], [NormalizedEmail], [EmailConfirmed], [PasswordHash], [SecurityStamp], [ConcurrencyStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEnd], [LockoutEnabled], [AccessFailedCount], [FirstName], [LastName], [ProfilePicture]) VALUES (N'791e56df-d6b3-438e-9a43-de4069b49606', N'admin', N'ADMIN', N'admin@gmail.com', N'ADMIN@GMAIL.COM', 0, N'AQAAAAEAACcQAAAAEMrLgH9GHV1aowg0VA8MUGbBpwt4Dhiqm5cuFR6isUmoBISr02kmjukxK1XPqxK8xg==', N'QB6Q4J5ZTN2GCLZEHDALNH5L27O3JHNS', N'12cf962a-a2f8-475b-989c-ef288865891f', NULL, 0, 0, NULL, 1, 0, N'HoaiNhan', N'LamHoai', NULL)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [security].[Users] WHERE Id = '791e56df-d6b3-438e-9a43-de4069b49606'");
+        }
+    }
+}
---
+++ Data/Migrations/20240528072206_AssignAdminUserToAllRoles.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserManagementWithIdentity.Data.Migrations
+{
+    public partial class AssignAdminUserToAllRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO [security].[UserRoles] (UserId, RoleId) SELECT '791e56df-d6b3-438e-9a43-de4069b49606', Id FROM [security].[Roles]");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [security].[UserRoles] WHERE UserId = '791e56df-d6b3-438e-9a43-de4069b49606'");
+        }
+    }
+}
